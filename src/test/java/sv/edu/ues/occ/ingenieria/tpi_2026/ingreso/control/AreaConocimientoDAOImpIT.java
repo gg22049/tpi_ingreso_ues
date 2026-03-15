c9f350fa29c7feb62e2bdf07814f534bed5ba759
gg22049 <gg22049@ues.edu.sv>
2026-03-15 10:14:25 -0600

    clases del persistence y version del plugin para compilar

--- src/test/java/sv/edu/ues/occ/ingenieria/tpi_2026/ingreso/control/AreaConocimientoDAOImpIT.java
+++ src/test/java/sv/edu/ues/occ/ingenieria/tpi_2026/ingreso/control/AreaConocimientoDAOImpIT.java
@@ -1,7 +1,7 @@
 package sv.edu.ues.occ.ingenieria.tpi_2026.ingreso.control;
 
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.AfterAll;
+import jakarta.persistence.EntityTransaction;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import static org.junit.jupiter.api.Assertions.*;
-import org.testcontainers.shaded.org.yaml.snakeyaml.emitter.Emitter;
+import sv.edu.ues.occ.ingenieria.tpi_2026.ingreso.entity.AreaConocimiento;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -24,23 +24,30 @@ import org.testcontainers.shaded.org.yaml.snakeyaml.emitter.Emitter;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AreaConocimientoDAOImpIT {
 
-    EntityManager em;
+    IntegrationTestSingleton its;
 
     @BeforeAll
     void start() {
-        IntegrationTestSingleton its = IntegrationTestSingleton.getInstance();
-        em = its.getEntityManager();
-    }
-
-    @AfterAll
-    void stop() {
-        em.close();
+        its = IntegrationTestSingleton.getInstance();
     }
 
     @Test
-    void test() {
-        System.out.println("AreaConocimientoDAOImp.create");
-        System.out.println(em.isOpen());
+    void testCreate() {
+        System.out.println("AreaConocimientoDAOImp.testCreate");
+        AreaConocimiento newEntity = new AreaConocimiento();
+        newEntity.setNombre("xd");
+        AreaConocimientoDAOImp cut = new AreaConocimientoDAOImp();
+        cut.em = its.getEntityManager();
+        EntityTransaction tx = cut.em.getTransaction();
+        try {
+            tx.begin();
+            cut.create(newEntity);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        }
+        assertNotNull(newEntity.getIdAreaConocimiento());
+        cut.em.close();
     }
 
 }
